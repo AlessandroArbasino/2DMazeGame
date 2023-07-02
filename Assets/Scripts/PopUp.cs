ff90331e2078ae183c62110cd319c776d484333e
Alessandro Arbasino <alessandroarbasino99@gmailcom>
2023-07-02 18:50:05 +0200

    Setup Menu scene

--- Assets/Scripts/PopUp.cs
+++ Assets/Scripts/PopUp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static PopUpManager;
 
@@ -13,7 +14,7 @@ public class PopUp : MonoBehaviour
     [SerializeField] private TextMeshProUGUI buttonFunctionText;
     [SerializeField] private Button functionalityButton;
     [SerializeField] private GameObject functionalityButtonContainer;
-    public void InitializeGenericPopUp(string message,string title,string functionalityButtonString)
+    public void InitializeGenericPopUp(string message, string title, string functionalityButtonString)
     {
         titleText.text = title;
         messageText.text = message;
@@ -22,7 +23,7 @@ public class PopUp : MonoBehaviour
         functionalityButtonContainer.SetActive(false);
     }
 
-    public void InitializeFunctionalytyPopUp(string message, string title, string functionalityButtonString, UnityAction function,bool closeOnActivation = true)
+    public void InitializeFunctionalytyPopUp(string message, string title, string functionalityButtonString, UnityAction function)
     {
         titleText.text = title;
         messageText.text = message;
@@ -30,14 +31,11 @@ public class PopUp : MonoBehaviour
         functionalityButton.onClick.AddListener(function);
         functionalityButtonContainer.SetActive(true);
 
-        if (closeOnActivation)
-            functionalityButton.onClick.AddListener(delegate { ClosePopPup(); });
+        functionalityButton.onClick.AddListener(delegate { BackToMainMenu(); });
     }
 
-    public void ClosePopPup()
+    public void BackToMainMenu()
     {
-        functionalityButton.onClick.RemoveAllListeners();
-        functionalityButtonContainer.SetActive(true);
-        gameObject.SetActive(false);
+        SceneManager.LoadScene("MenuScene");
     }
 }
